b8afed478cbdc8ba68631478ef47c92a9f4d07a2
Jack Knowles <jackmhknowles@gmail.com>
2022-07-16 12:35:24 +0100

    add merchant works

--- controllers/merchant_controller.py
+++ controllers/merchant_controller.py
@@ -6,14 +6,24 @@ import repositories.merchant_repository as merchant_repository
 
 merchants_blueprint = Blueprint("merchants", __name__)
 
-
 @merchants_blueprint.route("/merchants")
 def merchants():
     merchants = merchant_repository.select_all()
     return render_template("merchants/index.html", all_merchants=merchants)
 
 
+# NEW '/merchants/new'
 @merchants_blueprint.route("/merchants/new")
 def new_merchant():
     return render_template("merchants/new.html")
 
+
+# CREATE '/merchants'  [POST]
+@merchants_blueprint.route("/merchants", methods=["POST"])
+def create_merchant():
+    name = request.form['merchant-name']
+    merchant = Merchant(name)
+    merchant_repository.save(merchant)
+    
+    return redirect("/merchants")
+#
