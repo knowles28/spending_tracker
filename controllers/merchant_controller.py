6f33b356ebc88c93d381ba65132ffb1a548346c1
Jack Knowles <jackmhknowles@gmail.com>
2022-07-16 12:25:42 +0100

    set up basic form for New Merchant

--- controllers/merchant_controller.py
+++ controllers/merchant_controller.py
@@ -6,8 +6,14 @@ import repositories.merchant_repository as merchant_repository
 
 merchants_blueprint = Blueprint("merchants", __name__)
 
+
 @merchants_blueprint.route("/merchants")
 def merchants():
     merchants = merchant_repository.select_all()
     return render_template("merchants/index.html", all_merchants=merchants)
-        
+
+
+@merchants_blueprint.route("/merchants/new")
+def new_merchant():
+    return render_template("merchants/new.html")
+
