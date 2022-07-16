9040d46583416cb6db5025d2c7d40131ebf6e6dc
Jack Knowles <jackmhknowles@gmail.com>
2022-07-16 14:52:18 +0100

    stuck on update function

--- controllers/merchant_controller.py
+++ controllers/merchant_controller.py
@@ -9,11 +9,12 @@ merchants_blueprint = Blueprint("merchants", __name__)
 @merchants_blueprint.route("/merchants")
 def merchants():
     merchants = merchant_repository.select_all()
+    
     return render_template("merchants/index.html", all_merchants=merchants)
 
 
 # NEW '/merchants/new'
-@merchants_blueprint.route("/merchants/new")
+@merchants_blueprint.route("/merchants/new", methods=['GET'])
 def new_merchant():
     return render_template("merchants/new.html")
 
@@ -29,13 +30,27 @@ def create_merchant():
 
 
 #EDIT '/merchants/<id>/edit'
+@merchants_blueprint.route("/merchants/<id>/edit", methods=['POST'])
+def edit_merchant(id):
+    merchant = merchant_repository.select(id)
+    
+    return render_template("/merchants/edit.html", merchant=merchant)
 
 
 #UPDATE '/merchants' [PUT/POST?]
+@merchants_blueprint.route("/merchants/<id>", methods=['POST'])
+def update_merchant(id):
+    name = request.form['merchant-name']
+    merchant = Merchant(name, id)
+    
+    merchant_repository.update(merchant)
+    
+    return redirect("/merchants")
 
 
 #DELETE '/merchants/edit'? [POST]
 @merchants_blueprint.route("/merchants/<id>/delete", methods=['POST'])
 def delete_merchant(id):
     merchant_repository.delete(id)
+    
     return redirect('/merchants')
--- repositories/merchant_repository.py
+++ repositories/merchant_repository.py
@@ -2,12 +2,14 @@ from db.run_sql import run_sql
 
 from models.merchant import Merchant
 
+
 def save(merchant):
     sql = "INSERT INTO merchants (name) VALUES (%s) RETURNING *"
     values = [merchant.name]
     results = run_sql(sql, values)
     merchant.id = results[0]['id']
     return merchant
+
 
 def select_all():
     merchants = []
@@ -19,6 +21,27 @@ def select_all():
         merchants.append(merchant)
     
     return merchants
+
+def select(id):
+    merchant = None
+    sql = "SELECT * from merchants WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+    
+    # note to self - if statement without condition checks variable is not empty
+    if results:
+        result = results[0]
+        merchant = Merchant(result['name'], result['id'])
+    
+    return merchant
+        
+        
+
+def update(merchant):
+    sql = "UPDATE merchants SET (name) = (%s) WHERE id = %s"
+    values = [merchant.name, merchant.id]
+    run_sql(sql, values)
+
 
 def delete(id):
     sql = "DELETE FROM merchants WHERE id = %s"
