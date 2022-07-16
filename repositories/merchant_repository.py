6daf5d63b4f35a59c7b9666e46f21879a8cf05a4
Jack Knowles <jackmhknowles@gmail.com>
2022-07-16 11:40:21 +0100

    finished basic methods for merchant repository, will set up controller for that and get it  talking to frontend

--- repositories/merchant_repository.py
+++ repositories/merchant_repository.py
@@ -8,3 +8,20 @@ def save(merchant):
     results = run_sql(sql, values)
     merchant.id = results[0]['id']
     return merchant
+
+def select_all(merchant):
+    merchants = []
+    sql = "SELECT * FROM merchants"
+    results = run_sql(sql)
+    
+    for row in results:
+        merchant = Merchant(row['name'], row['id'])
+        merchants.append(merchant)
+    
+    return merchants
+
+def delete(id):
+    sql = "DELETE FROM merchants WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
